89b531537f971629bc1e832afbe07e02a618dfa6
Shipra Srivastava <code.shipra@gmail.com>
2020-02-23 12:29:44 +0530

    backbone code

--- app/app.py
+++ app/app.py
@@ -1,12 +1,55 @@
-from flask import Flask
+from datetime import datetime
+
+from elasticsearch import Elasticsearch
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+es = Elasticsearch()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/', methods=['GET'])
+def index():
+    results = es.get(index='contents', doc_type='title', id='my-new-slug')
+    return jsonify(results['_source'])
+
+
+@app.route('/add', methods=['POST'])
+def insert_data():
+    data = request.json
+    slug = data['slug']
+    title = data['title']
+    content = data['content']
+
+    body = {
+        'slug': slug,
+        'title': title,
+        'content': content,
+        'timestamp': datetime.now()
+    }
+
+    result = es.index(index='contents', doc_type='title', id=slug, body=body)
+
+    return jsonify(result)
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.json['keyword']
+
+    body = {
+        "query": {
+            "multi_match": {
+                "query": keyword,
+                "fields": ["content", "title"]
+            }
+        }
+    }
+
+    res = es.search(index="contents", doc_type="title", body=body)
+    return jsonify(res['hits']['hits'])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
